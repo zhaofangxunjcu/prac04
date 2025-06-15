b2c9142b372183bd5e3b2762ae2b433576a4d843
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-15 15:47:19 +0800

    Currently, main just prints data. Add a new function to display subject details that produces something like the following:
    CP1401 is taught by Ada Lovelace and has 192 students
    CP1404 is taught by Alan Turing  and has  98 students

--- subject_reader.py
+++ subject_reader.py
@@ -2,23 +2,22 @@
 CP1404/CP5632 Practical
 Data file -> lists program
 """
-
 FILENAME = "subject_data.txt"
-
-
 def main():
     data = load_data()
-    print(data)
-
+    display_subject_details(data)
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     data = []
     with open(FILENAME) as input_file:
         for line in input_file:
-            line = line.strip()  # Remove trailing newline
-            parts = line.split(',')  # Split into [subject, lecturer, number as string]
-            parts[2] = int(parts[2])  # Convert number to integer
-            data.append(parts)  # Add to list
+            line = line.strip()
+            parts = line.split(',')
+            parts[2] = int(parts[2])
+            data.append(parts)
     return data
-
+def display_subject_details(data):
+    """Display subject detail in a readable format."""
+    for subject, lecturer, student_count in data:
+        print(f"{subject} is taught by {lecturer} and has {student_count} students")
 main()
