ba9130f5d2cac801e40d9cc885ad048cffac5ff0
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-15 21:21:05 +0800

    print function refactor

--- total_income.py
+++ total_income.py
@@ -11,11 +11,16 @@ def main():
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
+    print_report(incomes, number_of_mouths)
+
+
+def print_report(incomes, number_of_mouths):
     total = 0
     for month in range(1, number_of_mouths + 1):
         income = incomes[month - 1]
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
 
 main()
 
