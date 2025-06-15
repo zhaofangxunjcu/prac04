2a497b91790031e29247a426e4b0990534a67700
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-15 21:18:53 +0800

    number_of_mouths

--- total_income.py
+++ total_income.py
@@ -5,14 +5,14 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
-    for month in range(1, months + 1):
+    number_of_mouths = int(input("How many months? "))
+    for month in range(1, number_of_mouths + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_mouths + 1):
         income = incomes[month - 1]
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
