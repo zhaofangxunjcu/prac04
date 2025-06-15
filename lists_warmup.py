dfd4a312e77a7c8cd090d3ce87e8175c68530804
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-15 15:29:04 +0800

    task2

--- lists_warmup.py
+++ lists_warmup.py
@@ -8,3 +8,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers     → False
 # "3" in numbers   → False (string vs integer)
 # numbers + [6, 5, 3] → [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+numbers[0] = "ten"         # Change first element to "ten"
+numbers[-1] = 1            # Change last element to 1
+
+print(numbers[2:])         # Print all elements except the first two
+print(9 in numbers)        # Check if 9 is in the list
