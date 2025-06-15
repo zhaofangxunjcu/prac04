2613de664b4cf4e00950d82ec54fdae6cca61a55
zfz20 <fangxun.zhao@my.jcu.edu.au>
2025-06-15 21:08:38 +0800

    Change the line that gets the income input so that it uses an f-string instead of string concatenation (+).

--- total_income.py
+++ total_income.py
@@ -7,14 +7,17 @@ def main():
     incomes = []
     months = int(input("How many months? "))
     for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
+
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
 main()
+
 # You can add values to a list using the .append() method.
 # two for loop
